03848a6802aa71eeca54abe33bc88f2382d7a841
licral <kurousagiwasugoi@gmail.com>
2017-05-11 11:38:30 +1200

    Added two buttons for register plant and add activity

--- src/views/calendarView.js
+++ src/views/calendarView.js
@@ -13,8 +13,11 @@ var calendarView = class CalendarView extends Component {
            <View style={styles.pageContent}>
                <Text>There should be a new button below</Text>
                <ActionButton buttonColor="rgba(255,0,0,1)">
-                    <ActionButton.Item buttonColor="#ff0000" title="Open Camera" onPress={() => {}}>
-                        <Icon name="add" style={styles.newButton} />
+                    <ActionButton.Item buttonColor="#ff0000" title="Record New Activity" onPress={() => {}}>
+                        <Icon name="create" style={styles.newButton} />
+                    </ActionButton.Item>
+                    <ActionButton.Item buttonColor="#ff0000" title="Register New Plant" onPress={() => {}}>
+                        <Icon name="local-florist" style={styles.newButton} />
                     </ActionButton.Item>
                </ActionButton>
            </View>
