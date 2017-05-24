d593fc50f79cd42f15865ccf31bde6dcf3ac1505
licral <kurousagiwasugoi@gmail.com>
2017-05-24 21:36:38 +1200

    Harvest activites can be recorded n ow

--- src/components/forms/HarvestForm.js
+++ src/components/forms/HarvestForm.js
@@ -18,8 +18,29 @@ const harvestForm = class HarvestForm extends Component{
         }
     }
 
-    record(){
-        console.log("Called in chemical!");
+    async record(activity_id){
+        var weight = this.state.weight;
+        try{
+            var TOKEN = await AsyncStorage.getItem(STORAGE_KEY);
+            fetch('https://orchard-app-java-tomcat.herokuapp.com/record/harvest', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Authorization': TOKEN
+                },
+                body: 'activity_id=' + encodeURIComponent(activity_id)
+                    + '&weight=' + encodeURIComponent(weight)
+            })
+            .then((response) => {
+                if(response.ok){
+                    this.props.navigateHome();
+                }
+            })
+            .done();
+        } catch (error) {
+            console.log("AsyncStorage error: " + error.message);
+        }
     }
 
     render () {
