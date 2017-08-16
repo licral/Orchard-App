a3b59b04117842f67f62ef84ed786d73415bbfc6
licral <kurousagiwasugoi@gmail.com>
2017-08-16 14:33:40 +1200

    Got options menu and sub menu working

--- src/components/OptionsButton.js
+++ src/components/OptionsButton.js
@@ -1,27 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import styles from '../styles/style.js';
 import {
     Menu,
     MenuOptions,
     MenuOption,
     MenuContext,
     MenuTrigger,
-    View
+    renderers
 } from 'react-native-popup-menu';
+import Modal from 'react-native-modal';
+import styles from '../styles/style.js';
 import {
-    TouchableOpacity
+    TouchableWithoutFeedback,
+    View,
+    Text
 } from 'react-native';
 
-const OptionsButton = ({ navigation }) => (
-    <Menu name="filterMenu">
-        <MenuTrigger>
-            <Icon name="more-vert" style={styles.headerRightIcon} />
-        </MenuTrigger>
-        <MenuOptions>
-            <MenuOption text='Test1' />
-        </MenuOptions>
-    </Menu>
-);
+const {SlideInMenu} = renderers;
+
+class OptionsButton extends Component{
+    constructor(){
+        super();
+        this.state = {
+            menuOpen: false
+        };
+    }
+
+    openMenu(value){
+        if(value == 1) {
+            this.setState({menuOpen: true});
+        }
+    }
+
+    render(){
+        return(
+            <View>
+                <Menu name="filterMenu" onSelect={value => this.openMenu(value)}>
+                    <MenuTrigger>
+                        <Icon name="more-vert" style={styles.headerRightIcon} />
+                    </MenuTrigger>
+                    <MenuOptions>
+                        <MenuOption value={1} text='Test1' />
+                    </MenuOptions>
+                </Menu>
+                <TouchableWithoutFeedback onPress={() => {this.setState({menuOpen: false})}}>
+                    <Modal isVisible={this.state.menuOpen} style={{justifyContent: 'flex-end', margin: 0}}>
+                        <TouchableWithoutFeedback onPress={() => {}}>
+                            <View style={{backgroundColor: 'white', padding: 22, justifyContent: 'center', alignItems: 'center'}}>
+                                <Text>This is a modal!</Text>
+                            </View>
+                        </TouchableWithoutFeedback>
+                    </Modal>
+                </TouchableWithoutFeedback>
+            </View>
+        );
+    }
+}
 
 export default OptionsButton;
