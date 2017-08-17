10c5e9312d89a54c2f71f3e65e9e55f57fe662a5
licral <kurousagiwasugoi@gmail.com>
2017-08-17 16:46:34 +1200

    Got the map module installed, but have yet to use

--- android/app/src/main/java/com/orchardapp/MainApplication.java
+++ android/app/src/main/java/com/orchardapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.orchardapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -27,8 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
-            new RCTCameraPackage(),
+            new MapsPackage(),
             new VectorIconsPackage(),
             new RCTCameraPackage()
       );
